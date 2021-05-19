ac312e2fbcb466c505a433387ab739341537ed53
Nit <83890233+nitiphong-nira@users.noreply.github.com>
2021-05-19 16:58:41 -0500

    Update rebalance_every_pair.py

--- rebalance_every_pair.py
+++ rebalance_every_pair.py
@@ -1,3 +1,19 @@
+#This code I create the library to rebalance between two crytrocurrencies as XLM and BTC, which is same idea with liqulity swap. Plase feel free to comment below my library
+#The rebalance model can apply to use more product which reduce more risk and let to asset allocation model.
+#I hope you enjoin to adjust my code. Moreover, you can use while loop to rebalance everyday same as liqudity-sawp in the binance or more frequency than 1 time a day.
+
+import pandas as pd
+import binance.client
+from binance.client import Client
+import time
+from binance.enums import *
+from binance.helpers import round_step_size
+from time import sleep
+
+api_key = "fill up by your self by https://www.binance.com/en/support/faq/360002502072"
+api_secret = "fill up by your self by https://www.binance.com/en/support/faq/360002502072"
+client = Client(api_key, api_secret)
+
 def after_rebalance(coin_1,coin_2,coin_pair):
     print("Your limited order is :")
     check = pd.DataFrame(data=client.get_open_orders(symbol=coin_pair))
